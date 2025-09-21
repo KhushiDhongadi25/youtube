1a23d225b56d9e59c525b3e7c3a3330bf396ad95
khushidhongadi25 <khushidhongadi25@gmail.com>
2025-09-21 18:07:16 +0530

    used youtube api and mapped videos, useEffect and fetch to fetch the api using api key, useState for reconciliation, destructure the info like title channelTitle view count from api fetched and displayed 50 videos by adding maxResults parameter in endpoint

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { YOUTUBE_VIDEO_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-    return (
-        <div>
-            <h1>VideoContainer</h1>
-        </div>
-    );
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEO_API);
+    const json = await data.json();
+    console.log(json.items);
+    setVideos(json.items);
+  };
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => {
+        return <VideoCard key={video.id} info={video} />;
+      })}
+    </div>
+  );
 };
 
 export default VideoContainer;
